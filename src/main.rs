adb3e1f9550c0f362e7e86cc2d8cec5ad516b166
n-inokawa <48897392+n-inokawa@users.noreply.github.com>
2020-08-23 02:14:49 +0900

    Hello with leds

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,34 @@
 #![no_std]
 #![no_main]
 
-// pick a panicking behavior
 use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-// use panic_abort as _; // requires nightly
-// use panic_itm as _; // logs messages over ITM; requires ITM support
-// use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
 
-use cortex_m::asm;
+use cortex_m::asm::delay;
 use cortex_m_rt::entry;
+use stm32f4::stm32f407;
 
 #[entry]
 fn main() -> ! {
-    asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
+    let p = stm32f407::Peripherals::take().unwrap();
+
+    p.RCC.ahb1enr.modify(|_, w| w.gpioden().set_bit());
+
+    let gpiod = &p.GPIOD;
+    gpiod.moder.modify(|_, w| w.moder12().output());
+    gpiod.moder.modify(|_, w| w.moder13().output());
+    gpiod.moder.modify(|_, w| w.moder14().output());
+    gpiod.moder.modify(|_, w| w.moder15().output());
 
     loop {
-        // your code goes here
+        delay(8_000_000);
+        gpiod.bsrr.write(|w| w.bs12().set_bit());
+        gpiod.bsrr.write(|w| w.bs13().set_bit());
+        gpiod.bsrr.write(|w| w.bs14().set_bit());
+        gpiod.bsrr.write(|w| w.bs15().set_bit());
+        delay(8_000_000);
+        gpiod.bsrr.write(|w| w.br12().reset());
+        gpiod.bsrr.write(|w| w.br13().reset());
+        gpiod.bsrr.write(|w| w.br14().reset());
+        gpiod.bsrr.write(|w| w.br15().reset());
     }
 }
